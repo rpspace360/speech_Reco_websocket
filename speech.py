3e340ee4f8bbd729594056464e7de192d8ccb238
rpsingh <ramprakash@codekraft.in>
2019-09-27 17:55:19 +0530

    adds feature with goggle cloud speech

--- speech.py
+++ speech.py
@@ -4,18 +4,20 @@ import datetime
 import random
 import websockets
 
-text = ""
 
 
 async def time(websocket, path):
+    
     r = sr.Recognizer()
     m = sr.Microphone()
     while True:
+        text = ""
         print("Say somethig!")
         with m as source:
-            r.adjust_for_ambient_noise(source, duration=3) 
-            r.pause_threshold = 0.5
-            audio = r.listen(source)
+            r.adjust_for_ambient_noise(source, duration=1) 
+            r.pause_threshold = 0.6
+            r.operation_timeout = 6
+            audio = r.listen(source=source, timeout=5)
             print("Got it! Now to recognize it...")
 
             # try:
@@ -28,14 +30,20 @@ async def time(websocket, path):
             try:
 
                 value = r.recognize_google(audio)
+                # value = r.recognize_google_cloud(audio_data=audio)
+                # value = r.recognize_bing(audio_data=audio, key="933596b75bdd4e418096876d7fa98635", region="westus")
                 text = value
                 print("You said: {}".format(text))
-                await websocket.send(text)
-                await asyncio.sleep(3)
+                
 
             except sr.UnknownValueError:
-                print("Oops")
-            now = datetime.datetime.utcnow().isoformat() + "Z"
+                print("Google Speech could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech service; {0}".format(e))
+            except sr.WaitTimeoutError as e:
+                print("Timeout; {0}".format(e))
+
+            await websocket.send(text)
             
 
 start_server = websockets.serve(time, "127.0.0.1", 5678)
